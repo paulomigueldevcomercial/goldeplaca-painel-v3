0f6950d31f8de7455345e492b186048e050c780e
Paulo Silva <paulomigueldevcomercial@gmail.com>
2026-04-14 22:33:40 -0300

    atualiza avatar

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -12,13 +12,14 @@ import {
 import { cilAccountLogout, cilSettings, cilUser } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
 import { clearStoredSession } from '../../utils/authSession'
 
 const AppHeaderDropdown = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const user = useSelector((state) => state.auth?.user)
+  const displayName = user?.name || user?.username || 'Usuário'
+  const userInitial = displayName.trim().charAt(0).toUpperCase()
 
   const handleLogout = () => {
     clearStoredSession()
@@ -37,11 +38,13 @@ const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        <CAvatar color="success" textColor="white" size="md">
+          {userInitial}
+        </CAvatar>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-body-secondary fw-semibold my-2">
-          {user?.name || user?.username || 'Usuário'}
+          {displayName}
         </CDropdownHeader>
         <CDropdownItem href="#">
           <CIcon icon={cilUser} className="me-2" />
