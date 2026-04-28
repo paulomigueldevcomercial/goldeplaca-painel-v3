73057bf50930f03e2aa62b108360addf25e3349b
Paulo Silva <paulomigueldevcomercial@gmail.com>
2026-04-28 00:28:08 -0300

    ajuste footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { CFooter } from '@coreui/react'
 
 const AppFooter = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <CFooter className="px-4">
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
+        <span className="me-1">Desenvolvido por</span>
         <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          Gol de Placa MA <span className="ms-1">&copy; 2025</span>
+          Gol de Placa MA <span className="ms-1">&copy; {currentYear}</span>
         </a>
       </div>
     </CFooter>
